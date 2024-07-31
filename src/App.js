fd27b6b9613356a4a7f1aba1a8984b6692bce75d
Vincent Moyaki <moyakivincent@yahoo.com>
2024-07-31 23:38:47 +0100

    WIP: Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
+import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div>
-      <header>
-      </header>
-    </div>
+      <Header />
   );
 }
 
